f405d3d11969d17e82e6ec851cc1854fd4c8a18f
Kayo <Kayohenriquesilva19@gmail.com>
2024-09-08 14:18:19 -0300

    feat: init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,31 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: {
+          default: '#DBAC2C',
+          light: '#F1E9C9',
+          dark: '#C47F17',
+        },
+        purple: {
+          default: '#8047F8',
+          light: '#EBE5F9',
+          dark: '#4B2995',
+        },
+        base: {
+          title: '#272221',
+          subtitle: '#403937',
+          text: '#574F4D',
+          label: '#8D8686',
+          hover: '#D7D5D5',
+          button: '#E6E5E5',
+          input: '#EDEDED',
+          card: '#F3F2F2',
+          background: '#FAFAFA',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
